88b5b3d90ce6586bf8dde3fda18478bb23243865
MinhTan <vienminhtanmd@gmail.com>
2025-05-15 23:49:35 +0700

    in-progress

--- Leetcode/Exercies/LinkedList.cs
+++ Leetcode/Exercies/LinkedList.cs
@@ -2,88 +2,93 @@
 
 namespace Leetcode.Exercies
 {
-    public class Node<T>
+    public class Node
     {
-        public T Data;
-        public Node<T> Next;
-        public Node<T> Previous;
+        public int Data;
+        public Node Next;
+        public Node Previous;
 
-        public Node(T value)
+        public Node(int value)
         {
             Data = value;
             Next = null;
         }
     }
 
-    public class LinkedList<T>
-    {
-        private Node<T> head;
+    //public class LinkedList
+    //{
+    //    private Node Head;
 
-        public void AddLast(T value)
-        {
-            Node<T> newNode = new Node<T>(value);
+    //    public LinkedList(Node Head)
+    //    {
+    //        this.Head = Head;
+    //    }
 
-            if (head == null)
-            {
-                head = newNode;
-                return;
-            }
-            else
-            {
-                Node<T> currentNode = head;
+    //    public void AddLast(int value)
+    //    {
+    //        Node  newNode = new Node(value);
+
+    //        if (Head == null)
+    //        {
+    //            Head = newNode;
+    //            return;
+    //        }
+    //        else
+    //        {
+    //            Node  currentNode = Head;
                 
-                while (currentNode.Next != null)
-                {
-                    currentNode = currentNode.Next;
-                }
+    //            while (currentNode.Next != null)
+    //            {
+    //                currentNode = currentNode.Next;
+    //            }
 
-                currentNode.Next = newNode;
-            }
-        }
+    //            currentNode.Next = newNode;
+    //        }
+    //    }
 
-        public void AddFirst(T value)
-        {
-            Node<T> newNode = new Node<T>(value);
+    //    public void AddFirst(int value)
+    //    {
+    //        Node  newNode = new Node(value);
 
-            newNode.Next = head;
-            head = newNode;
-        }
+    //        newNode.Next = Head;
+    //        Head = newNode;
+    //    }
 
-        public void RemoveFirst(T value)
-        {
-            if (head == null)
-                return;
+    //    public void RemoveFirst()
+    //    {
+    //        if (Head == null)
+    //            return;
 
-            head = head.Next;
+    //        Head = Head.Next;
 
-            return;
-        }
+    //        return;
+    //    }
 
-        public void RemoveLast(T value)
-        {
-            if (head == null)
-            {
-                return;
-            }
-            else
-            {
-                if (head.Next == null)
-                {
-                    head = null;
-                    return;
-                }
-                else
-                {
-                    Node<T> currentNode = head;
+    //    public void RemoveLast()
+    //    {
+    //        if (Head == null)
+    //        {
+    //            return;
+    //        }
+    //        else
+    //        {
+    //            if (Head.Next == null)
+    //            {
+    //                Head = null;
+    //                return;
+    //            }
+    //            else
+    //            {
+    //                Node  currentNode = Head;
 
-                    while (currentNode.Next != null)
-                    {
-                        currentNode = currentNode.Next;
-                    }
+    //                while (currentNode.Next != null)
+    //                {
+    //                    currentNode = currentNode.Next;
+    //                }
 
-                    currentNode.Next = null;
-                }
-            }
-        }
-    }
+    //                currentNode.Next = null;
+    //            }
+    //        }
+    //    }
+    //}
 }
--- Leetcode/Exercies/MergeTwoSortedList.cs
+++ Leetcode/Exercies/MergeTwoSortedList.cs
@@ -1,12 +1,79 @@
 ﻿using System;
+using System.Dynamic;
+using Leetcode.Exercies;
 
 namespace Leetcode.Exercies
 {
     public class MergeTwoSortedList
     {
-        MergeTwoSortedList()
+        private Node Head;
+
+        MergeTwoSortedList(Node FirstLinkedListHead, Node SecondLinkedListHead)
+        {
+            Head = (FirstLinkedListHead.Data > SecondLinkedListHead.Data) ? FirstLinkedListHead : SecondLinkedListHead;
+        }
+
+        public Node Merge()
         {
 
+
+
+            return null;
+        }
+
+        public void AddLast(Node Node)
+        { 
+            if (Head == null)
+            {
+                Head = Node;
+                return;
+            }
+            else
+            {
+                Node currentNode = Head;
+
+                while (currentNode.Next != null)
+                {
+                    currentNode = currentNode.Next;
+                }
+
+                currentNode.Next = Node;
+            }
+        }
+        public void AddFirst(Node Node)
+        {
+            Node.Next = Head;
+            Head = Node;
+        }
+
+        public void RemoveFirst()
+        {
+            if (Head == null)
+                return;
+
+            Head = Head.Next;
+        }
+
+        public void RemoveLast()
+        {
+            if (Head != null)
+            {
+                if (Head.Next == null)
+                {
+                    Head = null;
+                }
+                else
+                {
+                    Node currentNode = Head;
+
+                    while (currentNode.Next != null)
+                    {
+                        currentNode = currentNode.Next;
+                    }
+
+                    currentNode.Next = null;
+                }
+            }
         }
     }
 }
